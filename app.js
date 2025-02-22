d657f9bf26650a436568d5518c9394f8e29ad1ee
AKHILKUMAR111 <akhil.kumar.jakhmola5@gmail.com>
2025-02-22 15:23:39 +0530

    22nd feb - added seperation of concerns in models

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const ownersRouter = require("./routes/ownersRouter");
+const productsRouter = require("./routes/productsRouter");
+const usersRouter = require("./routes/usersRouter");
+
+
+const db = require("./config/mongoose-connection");
+const user = require("./models/user-model");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,"public")));
+app.set("view engione","ejs");
+
+app.use("/owners",ownersRouter);
+app.use("/users",usersRouter);  
+app.use("/products",productsRouter);
+
+
+
+
+
+
+
 
 
 app.get("/",(req,res)=>{
     res.send("working");
+})
+app.get("/products",(req,res)=>{
+    res.send("hey its working product");
+})
+
+app.get("/user",(req,res)=>{
+    res.send("hey its working product");
 })
 
 app.listen(3000);
